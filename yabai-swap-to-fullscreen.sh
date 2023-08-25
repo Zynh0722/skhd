fd1d4c3cd5cf58f83539fd3927ed29b3ee5d95f7
Zynh0722 <Zynh0722@gmail.com>
2023-08-25 09:09:01 -0700

    refactor with stewie410 advice

--- yabai-swap-to-fullscreen.sh
+++ yabai-swap-to-fullscreen.sh
@@ -1,26 +1,50 @@
-#!/bin/sh
-fullscreen_displays=$(yabai -m query --spaces | jq -c 'map(select(."is-native-fullscreen" == true))')
+#!/bin/bash
 
-if [ $(echo $fullscreen_displays | jq 'length') -gt 0 ]; then
+focus() {
+	yabai -m space --focus "$1"
+}
+
+get_fs_displays() {
+	yabai -m query --spaces | jq -c 'map(select(."is-native-fullscreen" == true))'
+}
+
+get_visible() {
+	printf '%s' "$1" | jq -c 'map(select(."is-visible" == true))'
+}
+
+is_empty() {
+	length="$(printf '%s' "$1" | jq 'length')"
+	[ "$length" -le 0 ]
+}
+
+first() {
+	printf '%s' "$1" | jq 'sort_by(.index) | .[0].index'
+}
+
+try_focus_visible() {
 	# This should be at max length 1. Though this may likely break in a multimonitor environment
-	visible_fullscreen_displays=$(echo $fullscreen_displays | jq -c 'map(select(."is-visible" == true))')
+	visible_fullscreen_displays=$(get_visible "$1")
 
-	goto_first_fs=false
-	if [ $(echo $visible_fullscreen_displays | jq 'length') -gt "0" ]; then
-		visible_id=$(echo $visible_fullscreen_displays | jq -c '.[0].index')
-		next_index=$(($visible_id + 1))
-		if [ $(echo $fullscreen_displays | jq "map(select(.index == $next_index)) | length") -gt "0" ]; then
-			yabai -m space --focus $next_index
-		else
-			goto_first_fs=true
+	if ! is_empty "$visible_fullscreen_displays"; then
+		visible_index=$(printf '%s' "$visible_fullscreen_displays" | jq -c '.[0].index')
+		next_index=$((visible_index + 1))
+		if [ "$(printf '%s' "$fullscreen_displays" | jq "map(select(.index == $next_index)) | length")" -gt "0" ]; then
+			focus "$next_index"
+			return 0
 		fi
-	else
-		goto_first_fs=true
 	fi
 
-	if $goto_first_fs; then
-		yabai -m space --focus $(echo $fullscreen_displays | jq 'sort_by(.index) | .[0].index')
-	fi
-else
-	yabai -m space --focus 1
-fi
+	return 1
+}
+
+main() {
+	fullscreen_displays="$(get_fs_displays)"
+
+	is_empty "$fullscreen_displays" && focus 1 && return
+
+	try_focus_visible "$fullscreen_displays" && return
+
+	focus "$(first "$fullscreen_displays")"
+}
+
+main
